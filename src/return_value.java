81afd5a20572c17391f1f6006a4028609dbc1833
MacBook M1 Air <macbookair@MacBooks-MacBook-Air.local>
2024-12-07 11:46:33 +0630

    master

---
+++ src/return_value.java
@@ -0,0 +1,21 @@
+
+public class return_value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  int result= calculate(12,20);
+		System.out.println(result);
+		String result1= names("Mg mg");
+		System.out.print(result1);
+
+	}
+	public static int calculate(int x, int y) {
+		int area = x * y;
+		return area;
+	}
+ public static String names(String name) {
+//	 String result = names;
+	 return name;
+ }
+	
+}
