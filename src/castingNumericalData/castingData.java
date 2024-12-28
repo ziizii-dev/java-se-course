d9929366c75a95780629f7db47299ddf395f3512
ziizii-dev <ziizii.dev@gmail.com>
2024-12-29 00:40:10 +0630

    master

---
+++ src/castingNumericalData/castingData.java
@@ -0,0 +1,19 @@
+package castingNumericalData;
+
+public class castingData {
+
+	public static void main(String[] args) {
+
+		 int x= 128;
+				 
+		  byte b= (byte)x;     //casting -128,-127,....0,1,...127
+		  float f= 234.4f;
+		  int y = (int)f;
+		  
+		  System.out.println(b);		 
+//		  System.out.println(y);
+
+		
+	}
+
+}
