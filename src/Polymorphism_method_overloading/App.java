43b86b779d37de12a2f219ba37db69f7d7c80eba
ziizii-dev <ziizii.dev@gmail.com>
2025-01-15 23:09:54 +0630

    master

--- src/Polymorphism_method_overloading/App.java
+++ src/Polymorphism_method_overloading/App.java
@@ -3,7 +3,6 @@ package Polymorphism_method_overloading;
 public class App {
 
 	public static void main(String[] args) {
-
        person person = new person();
 //       person.showInfo(34, "mag mag");
 //       person.showInfo(34);
