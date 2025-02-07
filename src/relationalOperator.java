3c646b00f74f2ba8fb2443c5e07d9d94d3fbb6e3
ziizii-dev <ziizii.dev@gmail.com>
2025-02-07 10:31:19 +0630

    master

--- src/relationalOperator.java
+++ src/relationalOperator.java
@@ -6,10 +6,10 @@ public class relationalOperator {
           int y =7;
           boolean z = x < y;
           System.out.println("z is " + z);
+          
           boolean i =x>y;
           System.out.println("i is " + i);
           boolean c = x==y;
-          
           System.out.println("c is " + c);
           boolean d = x!=y;
           System.out.println("d is " + d);
