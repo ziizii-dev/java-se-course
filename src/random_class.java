8d3a4415f81ddf6597d6b0ab9653cddc64bc4bd8
ziizii-dev <ziizii.dev@gmail.com>
2025-03-20 21:13:34 +0630

    master

--- src/random_class.java
+++ src/random_class.java
@@ -7,7 +7,6 @@ public class random_class {
      Random random = new Random();
      int number = random.nextInt();
      int number1 = random.nextInt(9);  //ans is between 0 to 9 always lessthan 9;
-
      double number2 = random.nextDouble();
      float number3 = random.nextFloat();
      System.out.println("Random:"+number);
@@ -15,7 +14,7 @@ public class random_class {
      System.out.println("Random:"+number2);   //it includes between 0 and 1;
      System.out.println("Random:"+number3);
      
-     
+
 	}
 	//to import shortkey use command shit o;
 
