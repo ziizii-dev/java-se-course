6cc78582abb19ef72937485f81bebab94f308861
ziizii-dev <ziizii.dev@gmail.com>
2025-04-09 05:29:12 +0630

    master

--- src/return_value.java
+++ src/return_value.java
@@ -12,7 +12,6 @@ public class return_value {
 	public static int calculate(int x, int y) {
 		int area = x * y;
 		return area;
-		
 	}
  public static String names(String name) {
 //	 String result = names;
