8b75abdd8e36797ce64df504b4892580c788888a
ziizii-dev <ziizii.dev@gmail.com>
2025-04-08 09:43:23 +0630

    master

--- src/return_value.java
+++ src/return_value.java
@@ -6,7 +6,8 @@ public class return_value {
 	  int result= calculate(12,20);
 		System.out.println(result);
 		String result1= names("Mg mg");
-		System.out.print(result1);
+		System.out.print
+		(result1);
 	}
 	public static int calculate(int x, int y) {
 		int area = x * y;
