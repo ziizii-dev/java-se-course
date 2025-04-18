c189a6ea21b17f813174b4e924623b4a6cb56975
ziizii-dev <ziizii.dev@gmail.com>
2025-04-18 12:40:39 +0630

    master

--- src/ifnested.java
+++ src/ifnested.java
@@ -5,6 +5,8 @@ public class ifnested {
 		// TODO Auto-generated method stub
 		int mark = 90;
 		if(mark >= 0 && mark <= 100) {
+
+			
 			if (mark >= 80) { 
 				System.out.println("Pass with Destiction");
 			}
