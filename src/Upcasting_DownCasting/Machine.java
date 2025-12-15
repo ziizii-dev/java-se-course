1c7e353f49dacb4ca9d00729335f66565caecf06
ziizii-dev <ziizii.dev@gmail.com>
2025-12-15 17:31:24 +0630

    master

--- src/Upcasting_DownCasting/Machine.java
+++ src/Upcasting_DownCasting/Machine.java
@@ -3,6 +3,7 @@ package Upcasting_DownCasting;
 public class Machine {
 	public void start() {
 		System.out.println("Machine start");
+		
 	}
 	public void stop() {
 		System.out.println("Machine stopped");
